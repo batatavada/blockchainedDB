fa46b209fba2868dc1bcd5871c6145f339ed1463
Abhishek <abbajpai@barc.gov.in>
2017-12-28 12:20:18 +0530

    added testHash.py

---
+++ testHash.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import hashlib
+
+
+# In[2]:
+
+hashlib.sha256("lala")
+
+
+# In[3]:
+
+hashlib.sha256("lala").hashdigest()
+
+
+# In[4]:
+
+hashlib.sha256("lala").hexdigest()
+
+
+# In[ ]:
+
+
+
